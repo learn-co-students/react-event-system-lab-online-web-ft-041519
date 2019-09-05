14905d7f4900238c262d9f879c2993c58991764e
Alison Golden <alisong779@yahoo.com>
2019-09-05 12:47:13 -0400

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,23 @@
-// Code EyesOnMe Component Here
+import React from 'react'
+
+  class EyesOnMe extends React.Component {
+
+    blur = () => {
+      console.log('Hey! Eyes on me!')
+    }
+
+    focus = () => {
+      console.log('Good!')
+    }
+
+
+    render(){
+      return (
+        <div>
+          <button onFocus={this.focus} onBlur={this.blur} value="button"/>
+        </div>
+      )
+    }
+  }
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from 'react'
+
+  class KeyPad extends React.Component {
+
+
+    render(){
+      return (
+        <div>
+          <input type="password" onKeyUp={() => console.log('Entering password...')}/>
+        </div>
+      )
+    }
+  }
+export default KeyPad;
